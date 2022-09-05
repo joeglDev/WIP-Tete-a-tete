e1da34020ec1394e48bc9c38375242a430bcefc5
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-09-05 21:10:28 +0100

    Refactor - remove status code from shared errsor mesages.

--- shared/http-errors.js
+++ shared/http-errors.js
@@ -1,8 +1,7 @@
 class HttpErrors {
-    static invalidLogin = {
-        status: 401,
-        msg: 'Invalid login credentials'
-    }
+  static invalidLogin = {
+    msg: "Invalid login credentials",
+  };
 }
 
 module.exports.HttpErrors = HttpErrors;

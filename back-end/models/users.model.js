ac6c020c39a42ec8dd54eaf5aba5d846955a9222
Hiroji <joegilbertdev@outlook.com>
2022-09-06 12:12:47 +0100

    selectUsersByUsername model half written

--- back-end/models/users.model.js
+++ back-end/models/users.model.js
@@ -1,7 +1,7 @@
 const db = require(`${__dirname}/../db/connection.js`);
 
 exports.selectUserByUsername = async (username) => {
-  //query(
+
   return await db.query("SELECT * FROM users WHERE username = $1", [username])
   
 

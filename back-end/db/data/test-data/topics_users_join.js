417c111007e160fde1b1be4ecb0bd5019990e8ec
solveigag <solveiga.g@outlook.com>
2022-09-07 17:15:48 +0100

    Add users-topics-join table which currently doesn't work consistently

---
+++ back-end/db/data/test-data/topics_users_join.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {user_id: 1, topic_id: 1},
+    {user_id: 1, topic_id: 2},
+    {user_id: 1, topic_id: 3},
+];
--- back-end/db/seeds/seed.js
+++ back-end/db/seeds/seed.js
@@ -1,12 +1,14 @@
 const format = require("pg-format");
 const db = require("../connection");
 
-
-const seed = async ({ usersData, topicsData }) => {
-
+const seed = async ({ usersData, topicsData, topics_users_joinData }) => {
+  await db.query(`DROP TABLE IF EXISTS users_topics_join;`);
   await db.query(`DROP TABLE IF EXISTS users;`);
   await db.query(`DROP TABLE IF EXISTS topics;`);
 
+// sometimes tables don't execute in correct order
+
+  //creates tables
 
   const usersTablePromise = db.query(`
   CREATE TABLE users (
@@ -23,35 +25,61 @@ const seed = async ({ usersData, topicsData }) => {
     topic_name VARCHAR(25) NOT NULL
   );`);
 
-  await Promise.all([usersTablePromise, topicsTablePromise]);
+  const users_topics_joinTablePromise = db.query(`
+  CREATE TABLE users_topics_join (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) NOT NULL,
+    topic_id INT REFERENCES topics(topic_id) NOT NULL
+  );`);
 
-  const insertUsersQueryStr = format( 
-    'INSERT INTO users ( username, screen_name, bio, img_url) VALUES %L RETURNING *;',
+  await Promise.all([usersTablePromise, topicsTablePromise])
+    .then(() => {
+      return users_topics_joinTablePromise;
+    })
+    .then(); 
+
+  // await users_topics_joinTablePromise;
+
+  // inserts data
+
+  const insertUsersQueryStr = format(
+    "INSERT INTO users ( username, screen_name, bio, img_url) VALUES %L RETURNING *;",
     usersData.map(({ username, screen_name, bio, img_url }) => [
       username,
       screen_name,
       bio,
-      img_url
+      img_url,
     ])
   );
-  
+
   const usersPromise = db
     .query(insertUsersQueryStr)
     .then((result) => result.rows);
 
-    const insertTopicsQueryStr = format( 
-      'INSERT INTO topics (topic_name) VALUES %L RETURNING *;',
-      topicsData.map(({ topic_name }) => [
-        topic_name
-      ])
-    );  
-    
-    const topicsPromise = db
-      .query(insertTopicsQueryStr)
-      .then((result) => result.rows);
+  const insertTopicsQueryStr = format(
+    "INSERT INTO topics (topic_name) VALUES %L RETURNING *;",
+    topicsData.map(({ topic_name }) => [topic_name])
+  );
 
-  await Promise.all([usersPromise, topicsPromise]);
-  
+  const topicsPromise = db
+    .query(insertTopicsQueryStr)
+    .then((result) => result.rows);
+
+  const insertUsers_Topics_JoinQueryStr = format(
+    "INSERT INTO users_topics_join (user_id, topic_id) VALUES %L RETURNING *;",
+    topics_users_joinData.map(({ user_id, topic_id }) => [user_id, topic_id])
+  );
+
+  const users_Topics_JoinPromise = db
+    .query(insertUsers_Topics_JoinQueryStr)
+    .then((result) => result.rows);
+
+  await Promise.all([usersPromise, topicsPromise])
+    .then(() => {
+      return users_Topics_JoinPromise;
+    })
+    .then();
+  // await users_Topics_JoinPromise;
 };
 
 module.exports = seed;

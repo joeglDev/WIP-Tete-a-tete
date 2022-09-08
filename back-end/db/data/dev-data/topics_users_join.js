512176bd1c02a1ae796d1bd21e15881e29024709
Hiroji <joegilbertdev@outlook.com>
2022-09-08 11:46:26 +0100

    join table now includes joins

--- back-end/db/data/dev-data/topics_users_join.js
+++ back-end/db/data/dev-data/topics_users_join.js
@@ -1,5 +1,12 @@
 module.exports = [
-    {user_id: 1, topic_id: 1},
-    {user_id: 1, topic_id: 2},
-    {user_id: 1, topic_id: 3},
+  { user_id: 1, topic_id: 1 },
+  { user_id: 1, topic_id: 2 },
+  { user_id: 1, topic_id: 3 },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
+  { user_id: 1, topic_id: null },
 ];

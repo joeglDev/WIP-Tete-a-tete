cc540f32ed49a68db893e967c8267b13104f744f
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-09-11 12:58:19 +0100

    Refactor - removed unused functions from models utils file

--- back-end/models/model-utils.js
+++ back-end/models/model-utils.js
@@ -16,38 +16,3 @@ exports.rejectWhenNonExistent = async (table, column, value) => {
   }
   return [];
 };
-
-exports.selectItemsWhere = async (table, column, value) => {
-  const { rows } = await db.query(
-    `SELECT * FROM ${table} WHERE ${column} = $1`,
-    [value]
-  );
-  return rows;
-};
-
-exports.getItem = async (table, column, value) => {
-  const [item] = await this.selectItemsWhere(table, column, value);
-  return item;
-};
-
-exports.itemExists = async (table, column, value) => {
-  const { rows } = await db.query(
-    `SELECT * FROM ${table} WHERE ${column} = $1`,
-    [value]
-  );
-  return rows.length !== 0;
-};
-
-exports.insertItem = async (table, column, value) => {
-  const { rows } = await db.query(
-    `INSERT INTO ${table}  (${column}) VALUES ($1) RETURNING *;`,
-    [value]
-  );
-  return rows[0];
-};
-
-exports.getItemAndInsertWhenNonExistent = async (table, col, val) => {
-  const item = await this.getItem(table, col, val);
-  if (item === undefined) return await this.insertItem(table, col, val);
-  return item;
-};

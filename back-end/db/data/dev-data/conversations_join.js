eef8cb2071e485bf29a515aab6a5592edb29b3e8
Hiroji <joegilbertdev@outlook.com>
2022-09-13 15:03:14 +0100

    alligned dev data with test data

--- back-end/db/data/dev-data/conversations_join.js
+++ back-end/db/data/dev-data/conversations_join.js
@@ -5,13 +5,13 @@ module.exports = [
     topic_id: 1,
   },
   {
-    user_id: 1,
+    user_id: 2,
     conversation_id: 2,
     topic_id: 2,
   },
   {
-    user_id: 2,
-    conversation_id: 1,
-    topic_id: 1,
+    user_id: 3,
+    conversation_id: 3,
+    topic_id: 3,
   },
 ];

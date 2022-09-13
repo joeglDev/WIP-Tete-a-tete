fef5fd43c66492ba767b19de46e453e154da68bd
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-09-13 08:59:09 +0100

    Add some skeleton socket code for joining room.

--- back-end/io/app.socket.js
+++ back-end/io/app.socket.js
@@ -2,12 +2,23 @@ const app = require(`${__dirname}/../app.js`);
 //const cors = require(`${__dirname}/../app.js`);
 const cors = require("cors");
 
-const http = require('http').Server(app);
-const socketIO = require('socket.io')(http, {
+const http = require("http").Server(app);
+const socketIO = require("socket.io")(http, {
   cors: {
-      origin:  '*'//"http://localhost:3000" 
-  }
+    origin: "*", //"http://localhost:3000"
+  },
 });
 
-console.log(socketIO)
+console.log(socketIO);
 
+socketIO.on("connection", (socketIO) => {
+  console.log("User connected.");
+  socketIO.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+
+  socketIO.on("join", (joinData) => {
+    console.log("User Joins", joinData);
+    socketIO.join(joinData.roomName);
+  });
+});

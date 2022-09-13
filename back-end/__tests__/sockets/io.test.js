27240988f8e713e290dfea0ef0049f3f90b9c030
Hiroji <joegilbertdev@outlook.com>
2022-09-13 10:57:53 +0100

    passes connection mock

--- back-end/__tests__/sockets/io.test.js
+++ back-end/__tests__/sockets/io.test.js
@@ -1,38 +1,35 @@
 const { createServer } = require("http");
+const { Server } = require("socket.io");
 const Client = require("socket.io-client");
-const app = require("../../app");
 const socketIO = require("../../io/app.socket");
+
 let io, serverSocket, clientSocket;
 
 beforeAll((done) => {
-  app.listen(() => {
-    //const port = httpServer.address().port;
-    //console.log(port)
-    clientSocket = new Client(`http://localhost:9090`);
-    //socketIO.on("connection", (socket) => {
-    //  serverSocket = socket;
-    //});
+  const httpServer = createServer();
+  io = new Server(httpServer);
 
-    console.log("socketIO", socketIO);
-    console.log("clientsocket", clientSocket)
-
+  httpServer.listen(() => {
+    const port = httpServer.address().port;
+    clientSocket = new Client(`http://localhost:${port}`);
+    io.on("connection", (socket) => {
+      serverSocket = socket;
+    });
     clientSocket.on("connect", done);
   });
 });
 
 afterAll(() => {
-  socketIO.close();
+  io.close();
   clientSocket.close();
 });
 
-
 describe("instances a new chat room which users can connect to", () => {
-  test("correctly handles a user join event", () => {
-    console.log("In test")
-    clientSocket.on("connected", (arg) => {
+  test("correctly handles a user join event", (done) => {
+    clientSocket.on("connection", (arg) => {
       expect(arg).toBe("Client connected");
       done();
     });
-    
+    serverSocket.emit("connection", "Client connected");
   });
 });

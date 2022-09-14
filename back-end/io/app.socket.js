7d0f55215c28552c2bc7ac78854e74dcc279a04f
Hiroji <joegilbertdev@outlook.com>
2022-09-14 12:08:55 +0100

    refactored sockets to change var name

--- back-end/io/app.socket.js
+++ back-end/io/app.socket.js
@@ -1,44 +1,37 @@
 const app = require(`${__dirname}/../app.js`);
-const {Port} = require("../../shared/Port");
-console.log(Port.mainPort)
+const { Port } = require("../../shared/Port");
+console.log(Port.mainPort);
 
-
-
-const http = require('http').Server(app);
-const cors = require('cors');
-const io = require('socket.io')(http, {
+const http = require("http").Server(app);
+const cors = require("cors");
+const io = require("socket.io")(http, {
   cors: {
-      origin: "*"
-  }
+    origin: "*",
+  },
 });
 
 app.use(cors());
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-  socket.emit("hello", "world")
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
+  socket.emit("hello", "world");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
 
   //create a room of a specific name
-  socket.on('createRoom', (roomData) => {
-    const roomName = `${roomData.title}-${roomData.conversation_id}`;
-    roomData.room_name = roomName;
+  socket.on("createRoom", (roomJoinData) => {
+    const roomName = `${roomJoinData.title}-${roomJoinData.conversation_id}`;
+    roomJoinData.room_name = roomName;
     socket.join(roomName);
     console.log(`User has joined room: ${roomName}`);
-    socket.emit("onRoomJoin", roomData)
-  })
-
+    socket.emit("onRoomJoin", roomJoinData);
+  });
 });
-
-
-
 
 http.listen(Port.socketPort, () => {
   console.log(`Server listening on ${Port.socketPort}`);
 });
-
 
 /*
 const cors = require("cors");
@@ -69,6 +62,3 @@ socketIO.on("connection", (socketIO) => {
 });
 
 */
-
-
-

b78a9e8f92e4522c9fc28c8d4ee9fde05fddaf46
ToddDevRepo <todd.norton75@gmail.com>
2022-09-30 15:22:00 +0100

    Config - add production seed data

---
+++ back-end/db/data/production-data/conversations.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {title: "18th century Asian Baking", body: "Succulent sugars and traditional fruit cordials."},
+    {title: "javaScript vs TypeScript", body: "STATIC TYPING! WE ALL LIKE IT!"},
+    {title: "Dog Walking", body: "Woof!"},
+];
---
+++ back-end/db/data/production-data/topics.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {topic_name: "Asian Baking"},
+    {topic_name: "Coding is Life"},
+    {topic_name: "Dog Walking"},
+];
